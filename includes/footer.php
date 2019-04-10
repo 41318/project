329a342e1d97eddb224b46aed4645cdc74dde0cc
turtle <dauletkazhi@gmail.com>
2019-04-10 17:49:51 +0600

    1

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<div class="blocks">
+		a
+	</div>
+	<div class="bod">
+		<div class="blocks">
+			b
+		</div>
+		<div class="blocks">
+		 	c
+		</div>
+	</div>
+	<footer>
+		
+	</footer>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+</head>
+<body style="background-color:black;">
+	<header class="header">
+		<div class="menu">
+			<button>Main page</button>
+			<div>
+				<button type="submit">About</button>
+			    <button>Contacts</button>
+			</div>
+		</div>
+	</header>
