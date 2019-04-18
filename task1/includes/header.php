dbcf065119db7353c9800f06c22abf1b77c45bd2
turtle <dauletkazhi@gmail.com>
2019-04-18 15:17:18 +0600

    asd

--- task1/includes/header.php
+++ task1/includes/header.php
@@ -2,16 +2,31 @@
 <html>
 <head>
 	<title>Site</title>
-	<link rel="stylesheet" type="text/css" href="main.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 </head>
-<body style="background-color:black;">
-	<header class="header">
-		<div class="menu">
-			<button>Main page</button>
-			<div>
-				<button type="submit">About</button>
-			    <button>Contacts</button>
+<body class="bg-dark container-fluid">
+	<header class="mx-1 my-2 p-3 pt-4 bg-secondary row">
+		<!-- <img src="Photos/Turtle.jpeg" class="rounded-circle col-md-2"> -->
+		<!-- Have to look, couldn't do right image-->
+		<div class="row col-md-7">
+			<div class="col-md-2">
+				<button class="btn btn-default bg-white">Main</button>
+			</div>
+			<div class="col-md-2">	
+				<button class="btn btn-default bg-white">About</button>
+			</div>
+			<div class="col-md-4">		
+				<button class="btn btn-default bg-white">Contacts</button>
+			</div>
+		</div>
+		<div class="row col-md-3">
+			<div class="col-md-3">
+				<input type="text" name="" placeholder="Search" >
+			</div>
+			<div class="col-md-1">
+				<button><i class="fa fa-search"></i></button>
 			</div>
 		</div>
 	</header>
+	<div>
